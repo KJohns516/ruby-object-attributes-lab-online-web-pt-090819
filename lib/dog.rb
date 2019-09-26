203ded64fede2850a35bbe66afd3367b92a355de
Kristan Johns <kristandjohns@gmail.com>
2019-09-26 22:02:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,2 @@
 class Dog 
   
-  def name=(dogs_name)
-      @this_dogs_name = dogs_name 
-    end 
-    
-    def name 
-      @this_dogs_name 
-    end 
-  end 
-  def breed 
-    @this_dogs_breed
-    end 
-  end 
-  
-  fido = Dog.new 
-  fido.name = "Fido"
-  puts fido.name
-  fido.breed = "Basset Hound"
-  puts fido.breed
