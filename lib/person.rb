960bbf6679676316ee34a4196cd986abc2089fde
Kristan Johns <kristandjohns@gmail.com>
2019-09-26 21:41:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,12 @@
 class Person
   def name=(name_of_person)
-      @name_of_person = 
-  
+      @name_of_person = person_name 
+    end 
+  def name 
+      @name_of_person
+  end 
+end 
+
+beyonce = Person.new 
+beyonce.name = "Beyonce"
+puts beyonce.name 
