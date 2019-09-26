e6e2873d0398b3f9c82859523650edbb2dfdebd8
Kristan Johns <kristandjohns@gmail.com>
2019-09-26 22:07:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,8 +10,12 @@ def breed=(breed)
 end
 def breed
     @breed
+  end 
 end 
 
 fido = Dog.new 
 fido.name = "Fido"
 puts fido.name
+basset hound = Dog.new
+basset_hound.breed = "Basset Hound"
+puts basset_hound.breed
